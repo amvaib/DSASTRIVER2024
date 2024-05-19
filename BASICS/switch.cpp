fb0af9f46e1c92a1836ca0517d1b4845a3660881
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-05-20 00:11:03 +0530

    SWITCH ADDED

---
+++ BASICS/switch.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int day = 3;
+
+    switch(day){
+        case 1: 
+        cout<< "MONDAY";
+        break;
+
+        case 2: 
+        cout<< "TUESDAY";
+        break;
+
+        case 3: 
+        cout<< "WEDNESDAY";
+        break;
+
+        case 4: 
+        cout<< "THURSDAY";
+        break;
+
+        default:
+        cout<<"INVALID";
+
+
+    }
+
+    return 0;
+}
