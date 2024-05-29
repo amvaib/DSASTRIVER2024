44d6e0c2f56d48a9b222731e383ec4d45885b763
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-05-29 23:58:41 +0530

    UNORDEREDSET

---
+++ CPPSTL/unorderedset.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    unordered_set<int> us;
+    //unique but not sorted
+
+    //lowerbound and upperbound function
+    //does not works, rest all functions are same 
+    //as above , it does not stores in any 
+    //particular order it has a better complexity
+    //than set in most cases, except some when collision happens
+}
