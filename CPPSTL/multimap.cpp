e97229bfceb0fa71e5a712f8feabb540af9c892b
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-05-31 00:32:07 +0530

    MULTIMAP AND UNORDERED MAP

---
+++ CPPSTL/multimap.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    multimap <int,int> mpp;
+
+    //EVERYTHING IS SAME AS MAP , ONLY IT CAN STORE MULTIPLE KEYS
+
+    //DUPLICATE KEYS
+    //SORTED ORDER
+    //ONLY mpp[key] cannot be used here
+
+    unordered_map<int, int> ump;
+
+    //UNORDERED
+    //UNIQUE KEYS
+
+    
+
+
+
+
+}
