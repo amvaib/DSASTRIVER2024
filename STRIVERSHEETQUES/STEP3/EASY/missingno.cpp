86f2336a761db4b7b1715e7d8dbf13b14b1ecd35
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-06-20 16:18:42 +0530

    ARRAY

--- STRIVERSHEETQUES/STEP3/EASY/missingno.cpp
+++ STRIVERSHEETQUES/STEP3/EASY/missingno.cpp
@@ -17,3 +17,26 @@
 //         return sum-s1;
 
 //     }
+
+
+//OPTIMAL APPROACH 2;
+
+// int missingNumber(int n, vector<int>& arr) {
+
+//         // Your code goes here
+        
+//         int xor1 = 0;
+//         for(int i =1 ; i<=n; i++){
+//             xor1 = xor1^i;
+//         }
+        
+//         int xor2 = 0;
+//         for(int i = 0; i<n-1; i++){
+//             xor2 = xor2^arr[i];
+            
+//         }
+        
+//         return xor1^xor2;
+        
+        
+//     }
