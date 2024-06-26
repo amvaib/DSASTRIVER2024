81c4b1f5c4feca89007418bce159a741a674e5da
Vaibhav Gautam <vaibhavlovescode@gmail.com>
2024-06-27 00:49:14 +0530

    MEDIUM ARRAY

---
+++ STRIVERSHEETQUES/STEP3/EASY/sortedrotated.cpp
@@ -0,0 +1,11 @@
+ bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        for(int i  = 0; i<n ; i++){
+            if(nums[i]>nums[(i+1)%n]){
+                count++;
+
+            }
+        }
+        return (count <= 1);
+    }
--- STRIVERSHEETQUES/STEP3/MEDIUM/twosum.cpp
+++ STRIVERSHEETQUES/STEP3/MEDIUM/twosum.cpp
@@ -1,13 +1,16 @@
- bool check(vector<int>& nums) {
-        int count = 0;
+vector<int> twoSum(vector<int>& nums, int target) {
+        map<int  , int> mpp;
         int n = nums.size();
-        for(int i  = 0; i<n ; i++){
-            if(nums[i]>nums[(i+1)%n]){
-                count++;
-
+        for(int i=0 ; i<n; i++){
+            int num = nums[i];
+            int more = target-num;
+            if(mpp.find(more)!= mpp.end()){
+                return {mpp[more], i};
             }
-        }
-        return (count <= 1);
-    }
 
+            mpp[num] = i;
+        }
+
+        return{-1,-1};
+    }
 
